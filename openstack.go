dfa21c902aeec0aa4ad77cc73c7af832f3e48382
i0Ek3 <kno30826@gmail.com>
2020-10-12 16:26:39 +0800

    Change the reciver

--- openstack.go
+++ openstack.go
@@ -10,8 +10,8 @@ import (
 )
 
 const (
-    VA = 1 << 10
-    VB = 1 << 6
+	VA = 1 << 10
+	VB = 1 << 6
 )
 
 type Elem struct {
@@ -23,8 +23,8 @@ type Ostack struct {
 	entranced bool
 	fenced    []bool
 	size      int
-    cap_      int
-    expcap    []int
+	cap_      int
+	expcap    []int
 	begin     int
 	bottom    *Elem
 	top       *Elem
@@ -50,24 +50,24 @@ type Openstack interface {
 	GetMap(index int) (*Elem, []interface{})
 	Destroy(index int)
 	IsExpand(expanded bool) bool
-	Expand() []Ostack
-    expand() (newcap int) 
-    abs_(a, b int) int
+	Expand() []*Ostack
+	expand() (newcap int)
+	abs_(a, b int) int
 }
 
-func (o Ostack) init() {
+func (o *Ostack) init() {
 	o.entranced = true
 	for i := 0; i < o.size; i++ {
 		o.fenced[i] = false
 	}
 	o.size = 0
-    o.cap_ = 0
-    
-    exp := VA 
-    for i := 0; i < VB; i++ {
-        exp *= (i + 1)
-        o.expcap[i] = exp
-    }
+	o.cap_ = 0
+
+	exp := VA
+	for i := 0; i < VB; i++ {
+		exp *= (i + 1)
+		o.expcap[i] = exp
+	}
 	o.begin = 0
 	o.bottom = nil
 	o.top = nil
@@ -76,19 +76,19 @@ func (o Ostack) init() {
 	o.expanded = false
 }
 
-func (o Ostack) Size() int {
+func (o *Ostack) Size() int {
 	if o.empty {
 		return 0
 	}
 	return o.size
 }
 
-func (o Ostack) List() []*Elem {
-    // the default value of cap_ is 0, but when we create new
-    // slice, the value of cap_ will increased 1.5 times than
-    // o.size, this action make sure the expand can be proces
-    // -sed correctly.
-	store := make([]*Elem, o.size, (3 / 2) * o.size)
+func (o *Ostack) List() []*Elem {
+	// the default value of cap_ is 0, but when we create new
+	// slice, the value of cap_ will increased 1.5 times than
+	// o.size, this action make sure the expand can be proces
+	// -sed correctly.
+	store := make([]*Elem, o.size, (3/2)*o.size)
 	if !o.IsEmpty() {
 		for key, value := range o._map {
 			store[key] = value
@@ -97,21 +97,21 @@ func (o Ostack) List() []*Elem {
 	return store
 }
 
-func (o Ostack) GetBottom() *Elem {
+func (o *Ostack) GetBottom() *Elem {
 	if o.empty {
 		return nil
 	}
 	return o.bottom
 }
 
-func (o Ostack) GetTop() *Elem {
+func (o *Ostack) GetTop() *Elem {
 	if o.empty {
 		return nil
 	}
 	return o.top
 }
 
-func (o Ostack) AddElem(e *Elem) (added bool) {
+func (o *Ostack) AddElem(e *Elem) (added bool) {
 	if e.position < 0 || e.position > o.size {
 		added = false
 		log.Warnf("Cannot add element cause of invalid index.")
@@ -133,7 +133,7 @@ func (o Ostack) AddElem(e *Elem) (added bool) {
 	return
 }
 
-func (o Ostack) RemoveElem(e *Elem) (removed bool) {
+func (o *Ostack) RemoveElem(e *Elem) (removed bool) {
 	if o.empty {
 		removed = false
 	}
@@ -146,7 +146,7 @@ func (o Ostack) RemoveElem(e *Elem) (removed bool) {
 }
 
 // isEmpty checks if the openstack is empty.
-func (o Ostack) IsEmpty() bool {
+func (o *Ostack) IsEmpty() bool {
 	if o.empty {
 		return true
 		log.Warnf("Openstack is empty, please add some elements.")
@@ -154,14 +154,14 @@ func (o Ostack) IsEmpty() bool {
 	return false
 }
 
-func (o Ostack) Check(err error) {
+func (o *Ostack) Check(err error) {
 	if err != nil {
 		panic(err)
 	}
 }
 
 // isExist checks if the element e exist.
-func (o Ostack) IsExist(e *Elem) bool {
+func (o *Ostack) IsExist(e *Elem) bool {
 	if o.__map[e] != nil && e != nil {
 		return true
 	} else if o.__map[e] == nil && e != nil {
@@ -172,7 +172,7 @@ func (o Ostack) IsExist(e *Elem) bool {
 
 // When openstack's fence status is true, we turn down the entrance
 // and start to mapping elements.
-func (o Ostack) SetMap(e *Elem, v ...interface{}) (mapped []bool) {
+func (o *Ostack) SetMap(e *Elem, v ...interface{}) (mapped []bool) {
 	for k := 0; k < o.size; k++ {
 		if !o.fenced[k] {
 			mapped[k] = false
@@ -182,16 +182,16 @@ func (o Ostack) SetMap(e *Elem, v ...interface{}) (mapped []bool) {
 	}
 	// Here means element e mapped to any value but with slice, for
 	// now, this map indicate the related resource of element e.
-    if len(v) > 0 {
-        o.__map[e] = v[0].([]interface{})
-    }
-    o.__map[e] = v
+	if len(v) > 0 {
+		o.__map[e] = v[0].([]interface{})
+	}
+	o.__map[e] = v
 
 	return
 }
 
 // getMap gets the value mapped by index and return it's mapping value.
-func (o Ostack) GetMap(index int) (e *Elem, v []interface{}) {
+func (o *Ostack) GetMap(index int) (e *Elem, v []interface{}) {
 	if index < 0 || index > o.size {
 		log.Warnf("Invalid index.")
 	}
@@ -206,7 +206,7 @@ func (o Ostack) GetMap(index int) (e *Elem, v []interface{}) {
 // and add into tail. But we have follows cases:
 //		1) the element's memory cannot occupied before the occupied one
 //		2) the map full of elements, we just delete the index mapped to
-func (o Ostack) Destroy(index int) {
+func (o *Ostack) Destroy(index int) {
 	if !o._map[index].allocated {
 		log.Warnf("This position have no element, you needn't to delete it!")
 	}
@@ -217,7 +217,7 @@ func (o Ostack) Destroy(index int) {
 	delete(o.__map, o._map[index])
 }
 
-func (o Ostack) IsExpand(expanded bool) bool {
+func (o *Ostack) IsExpand(expanded bool) bool {
 	if expanded {
 		return true
 	}
@@ -227,40 +227,40 @@ func (o Ostack) IsExpand(expanded bool) bool {
 // Expand expands capacity of openstack, first we create double space for
 // openstack then move original elements to new space, after then delete
 // original space.
-func (o Ostack) Expand() []Ostack {
-    newstack := make([]Ostack, 2 * o.size)
-    newstack = append(newstack, o)
-    for i := 0; i < o.size; i++ {
-        o.Destroy(i)
-    }
+func (o *Ostack) Expand() []*Ostack {
+	newstack := make([]*Ostack, 2*o.size)
+	newstack = append(newstack, o)
+	for i := 0; i < o.size; i++ {
+		o.Destroy(i)
+	}
 	return newstack
 }
 
-func (o Ostack) expand() (newcap int) {
-    if o.begin > o.size {
-        need := o.begin - o.size
-        if need <= 2 * o.cap_ { 
-            if o.cap_ < VA {
-                o.cap_ *= 2
-            } else {
-                o.cap_ *= (5 / 4)
-            }    
-        } else {
-            i := 0
-            o.cap_ = o.expcap[i]
-            if o.cap_ < need {
-                i++
-                o.cap_ = o.expcap[i] 
-            }
-        }
-    } 
-    newcap = o.cap_
-    return  
+func (o *Ostack) expand() (newcap int) {
+	if o.begin > o.size {
+		need := o.begin - o.size
+		if need <= 2*o.cap_ {
+			if o.cap_ < VA {
+				o.cap_ *= 2
+			} else {
+				o.cap_ *= (5 / 4)
+			}
+		} else {
+			i := 0
+			o.cap_ = o.expcap[i]
+			if o.cap_ < need {
+				i++
+				o.cap_ = o.expcap[i]
+			}
+		}
+	}
+	newcap = o.cap_
+	return
 }
 
-func (o Ostack) abs_(a, b int) int {
-    if a > b {
-        return a - b
-    }
-    return b - a
+func (o *Ostack) abs_(a, b int) int {
+	if a > b {
+		return a - b
+	}
+	return b - a
 }
